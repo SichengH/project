d9cbb75747bb889385a73d20d16bcbf83c0a7f2e
Sicheng Hao <hao.sic@husky.neu.edu>
2018-06-07 18:55:50 -0400

    data transfer

--- alzheimer.R
+++ alzheimer.R
@@ -16,11 +16,11 @@ library(ggplot2)
 
 
 ################## Read in data ##############
-height.freq<-fread(file = "GIANT_HEIGHT_Wood_et_al_2014_publicrelease_HapMapCeuFreq.txt")
+height.freq<-fread(file = "./A_nature/GIANT_EXTREME_BMI_Stage1_Berndt2013_publicrelease_HapMapCeuFreq.txt")
 height.enrichment<-fread(file = "GIANT_HEIGHT_Wood_et_al_2014_depict_gene_set_enrichment.txt")
 
-cis<-fread(file = "./eqtl-ramasay/2012-12-21-CisAssociationsProbeLevelFDR0.5.txt")
-trans<-fread(file = "./eqtl-ramasay/2012-12-21-TransEQTLsFDR0.5.txt",sep = "\t")#not working
+cis<-fread(file = "./A_nature/eqtl/2012-12-21-CisAssociationsProbeLevelFDR0.5.txt")
+trans<-fread(file = "./A_nature/eqtl/2012-12-21-TransEQTLsFDR0.5.txt",sep = "\t")#not working
 
 #load(file = "./eqtl-ramasay/expr.maps.rda")
 
@@ -31,8 +31,13 @@ trans<-fread(file = "./eqtl-ramasay/2012-12-21-TransEQTLsFDR0.5.txt",sep = "\t")
 
 
 
+################# Data transfer for SMR.exe############
 
-
+GWAS.sum<-fread(file = "./A_nature/GIANT_EXTREME_BMI_Stage1_Berndt2013_publicrelease_HapMapCeuFreq.txt")
+GWAS.sum<-GWAS.sum%>%mutate(n = N_cases+N_controls)%>%
+  select(SNP=MarkerName,A1=Allele1,A2=Allele2,freq=Allele1_Freq_HapMapCEU,b=beta,se=se,p=P,n=n)
+GWAS.sum<-GWAS.sum[1:1000,]
+save(GWAS.sum,file = "./A_nature/mygwas.ma")
 
 
 
